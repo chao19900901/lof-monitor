3e2e6a0276c376adcf5c1dd18d08ee7458490211
chao19900901 <53899608+chao19900901@users.noreply.github.com>
2026-05-12 17:05:49 +0800

    Update lof_notify.py

--- lof_notify.py
+++ lof_notify.py
@@ -590,7 +590,7 @@ def main():
 
     load_dotenv()  # 优先从本地 .env 文件加载 SERVERCHAN_KEY
 
-    sendkeys = ["SCT348643TPeDG7b88AeaCEbpc4uqvPKv2","SCT348625TeaJCpA5WwJh1WDaoGcZe1BwT"]
+    sendkeys = ["SCT348643TPeDG7b88AeaCEbpc4uqvPKv2","SCT348625TeaJCpA5WwJh1WDaoGcZe1BwT","SCT348719TRYV90SZ6SujEIaE4bwMFub7Q"]
     sendkey ="123"
     # for i in ["", "1", "2", "3", "4", "5"]:
     #     sendkey = os.environ.get(f"SERVERCHAN_KEY{i}", "").strip()
